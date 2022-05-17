ba2d8edb01a6498ea26f186a11c0da8cefcc7a09
htl3r-kre <0147@htl.rennweg.at>
2022-05-17 13:21:36 +0200

    EncP constructor + (MainTest)

--- src/EncryptedP.java
+++ src/EncryptedP.java
@@ -1,9 +1,17 @@
 import java.util.ArrayList;
 
 public class EncryptedP {
-    public String value;
-
-    public EncryptedP(String e) {
-
+    protected String value;
+    protected ArrayList<String> tags;
+    protected String userName;
+    public EncryptedP(String... args) {
+        if (args.length >= 3) {
+            this.value = args[0];
+            this.userName = args[1];
+            tags = new ArrayList<>();
+            for (int i = 2; i < args.length - 1; i++) {
+                tags.add(args[i]);
+            }
+        }
     }
 }
