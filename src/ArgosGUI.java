b4eb8c3636629b6130a0dccd0cc61380bf292e9a
paulwaldecker <waldeckerpaul@gmail.com>
2022-05-20 11:55:48 +0200

    ArgosGUI 1.0

--- src/ArgosGUI.java
+++ src/ArgosGUI.java
@@ -1,2 +1,30 @@
-public class ArgosGUI {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+
+public class ArgosGUI extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group group = new Group();
+        Scene scene = new Scene(group);
+        stage.setScene(scene);
+        stage.setTitle("Argos");
+        stage.setWidth(775D);
+        stage.setHeight(500D);
+        stage.centerOnScreen();
+
+        Label labelArgos = new Label("Argos");
+        labelArgos.setLayoutX(10);
+        labelArgos.setLayoutY(10);
+
+        group.getChildren().addAll(labelArgos);
+        stage.show();
+    }
 }
